c5fba042687089ba8b8377cb700f48ef19f649cf
Milan Košťák <milankostak@seznam.cz>
2017-04-07 20:38:10 +0200

    support for multiple WS clients

--- server.js
+++ server.js
@@ -4,33 +4,49 @@ const bodyParser = require('body-parser');
 const serveStatic = require('serve-static');
 
 const PORT = process.env.PORT || 3000;
-
-var wsClient = null;
+const wsClients = [];
 
 const server = express()
 	.use(serveStatic(__dirname, {'controller': false}))
 	.use(serveStatic(__dirname, {'receiver': false}))
 	.use(bodyParser.json())
-	.post('/show', (req, res) => {
-		// console.log(req.body);
-		if (wsClient != null) {
-			wsClient.send(JSON.stringify(req.body));
-		}
-		res.sendStatus(200);
-	})
-	.listen(PORT, () => console.log(`App server running on ${ PORT }`));
-
+	.post('/show', sendToClients)
+	.listen(PORT, () => console.log(`App server is running.\nPort number: ${ PORT }`));
 
 const wsServer = new WebSocket.Server({ server });
-console.log("WS server running.");	
+console.log("WS server is running.");
 
 wsServer.on('connection', (ws) => {
-	wsClient = ws;
+	wsClients.push(ws);
+	var host = ws['upgradeReq']['headers']['host'];
+
+	console.log("WS: client connected from ", host);
+	console.log("WS: number of clients: ", wsClients.length);
+
+	// received message from client back to server
 	ws.on('message', (message) => {
-		console.log('Received msg from WS client: %s', message);
+		console.log('WS: received message from client %s: %s', host, message);
 	});
+
+	// event for closing, remove closing client from array of active clients
 	ws.on('close', (message) => {
-		//console.log("WS connection closing: ", message);
-		wsClient = null;
+		//console.log("WS: connection closing: ", message);
+		console.log("WS: client closing ", host);
+		let index = wsClients.indexOf(ws);
+		if (index > -1) {
+			wsClients.splice(index, 1);
+		}
+		console.log("WS: number of clients: ", wsClients.length);
 	});
 });
+
+function sendToClients(req, res) {
+	res.sendStatus(200);
+	// console.log(req.body);
+	var data = JSON.stringify(req.body);
+
+	wsClients.forEach((client) => {
+		client.send(data);
+	});
+
+}
