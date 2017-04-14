5e57a65b35450a785509b1f817cfe29e8bfed0fb
Milan Košťák <milankostak@seznam.cz>
2017-04-14 13:47:12 +0200

    Math2: translated to English

--- js/math-extends.js
+++ js/math-extends.js
@@ -1,54 +1,55 @@
 "use strict";
 /**
- * Objekt obsahující pokročilé matematické funkce,
- *  které nejsou v klasickém Math obsaženy
+ * Object implements mathematical functions that are not implemented by classic Math object
  * @type {Object}
  * @author Milan Košťák
  */
 var Math2 = {};
 
 /**
- * Převod čísla ve stupních na radiány
- * @param  {number} degrees číslo ve stupních
- * @return {number}         číslo v radiánech
+ * Convert number in degrees to radians
+ * @param  {number} degrees number in degrees
+ * @return {number}         number in radians
  */
 Math2.toRadians = function(degrees) {
 	return degrees * Math.PI / 180;
 };
 
 /**
- * Převod čísla v radiánech na stupně
- * @param  {number} radians číslo v radiánech
- * @return {number}         číslo ve stupních
+ * Convert number in radians to degrees
+ * @param  {number} radians number in radians
+ * @return {number}         number in degrees
  */
 Math2.toDegrees = function(radians) {
 	return radians / Math.PI * 180;
 };
 
 /**
- * Znaménko čísla, signum funkce
- * @param  {number} x
- * @return {number}   1, 0, nebo -1
+ * A number representing the sign of the given argument
+ * The function fully simulates behaviour of the new Math.sign
+ * @deprecated use built-in Math.sign when possible
+ * @param  {number} x number to get the sign of
+ * @return {number}   NaN, 1, 0, -0, or -1
  */
 Math2.sign = function(x) {
 	return x > 0 ? 1 : (x < 0 ? -1 : 0);
 };
 
 /**
- * Oříznutí čísla
- * @param  {number} number číslo pro oříznutí
- * @param  {number} min    povolené minimum
- * @param  {number} max    povolené maximum
- * @return {number}        oříznuté číslo
+ * Clamp number by minimum and maximum
+ * @param  {number} number number to be clamped
+ * @param  {number} min    allowed minimum
+ * @param  {number} max    allowed maximum
+ * @return {number}        clamped number
  */
 Math2.clamp = function(number, min, max) {
 	return Math.min(Math.max(number, min), max);
 };
 
 /**
- * Zjištění desetinné části čísla
- * @param  {number} x vstupní číslo
- * @return {number}   desetinná část
+ * Get the fraction part of the given argument
+ * @param  {number} x input number
+ * @return {number}   fraction part of input
  */
 Math2.fpart = Math2.fract = function(x) {
 	return x % 1;
