403931f42f89569467a458c14a1df0ddca662789
Milan Košťák <milankostak@seznam.cz>
2017-04-30 11:41:09 +0200

    server port revert

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const serveStatic = require('serve-static');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const wsClients = [];
 
 const server = express()
