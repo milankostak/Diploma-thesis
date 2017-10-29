0bbbe221d4fad4371111ae0985f6928d6fe5f8a9
Milan Košťák <milankostak@seznam.cz>
2017-10-29 17:39:39 +0100

    added support for sending motion acceleration data

---
+++ js/motion.js
@@ -0,0 +1,169 @@
+"use strict";
+
+/**
+ * Motion object holds all necessary functions for starting and ending of listening for DeviceMotionEvent.
+ * It is assigned by its inner anonymous self-invoking function. By using this approach some variables and functions can remain private.
+ *
+ * @public
+ * @type {Object}
+ * @author Milan Košťák
+ * @version 1.0
+ */
+var Motion = (function() {
+
+	/**
+	 * Main object which is exported into public Motion variable.
+	 * @public
+	 * @type {Object}
+	 */
+	let Motion = {};
+
+	/**
+	 * Only if motion event is supported then it is possible to assign listener.
+	 * Support is checked inside init function.
+	 * @private
+	 * @type {Boolean}
+	 */
+	let isSupported = false;
+
+	/**
+	 * Purpose of sequence is to be able to sort data on the other side or discard delayed ones.
+	 * @private
+	 * @type {Number}
+	 */
+	let sequence = 0;
+
+	/**
+	 * Only every second set of motion data is being sent.
+	 * Therefore these three variables hold previous values and later are used to calculate mean with the next values.
+	 * Mean of two values is being sent.
+	 * @private
+	 * @type {Number}
+	 */
+	let tmpX = 0;
+	let tmpY = 0;
+	let tmpZ = 0;
+
+	/**
+	 * If an error occurs this variable prevents from showing the information multiple times.
+	 * @private
+	 * @type {Boolean}
+	 */
+	let errorShown = false;
+
+	/**
+	 * Holds information if program is currently listening for DeviceMotionEvent.
+	 * Prevents multiple adding of this event.
+	 * @private
+	 * @type {Boolean}
+	 */
+	let isRunning = false;
+
+	/**
+	 * Relative address on which the server is listening for motion data.
+	 * It is set by init function.
+	 * @type {String}
+	 */
+	let address;
+
+	/**
+	 * Main initialization  function.
+	 * Apart from setting the address, it checks if DeviceMotionEvent is supported by browser.
+	 * @param  {String} addr address on which the server is listening for motion data
+	 */
+	Motion.init = function(addr) {
+		address = addr;
+		if (window.DeviceMotionEvent) {
+			console.log("DeviceMotion is supported.");
+			isSupported = true;
+		} else {
+			console.log("%cDeviceMotion is not supported.", 'color: red');
+			window.alert("This device doesn't have all necessary sensors to determine its motion.");
+			isSupported = false;
+		}
+	};
+
+	/**
+	 * Function adds listener for DeviceMotionEvent - only if it is supported and not added already.
+	 * @public
+	 */
+	Motion.start = function() {
+		if (isSupported && !isRunning) {
+			window.addEventListener("devicemotion", deviceMotionHandler, false);
+			isRunning = true;
+		} else if (!isSupported) {
+			console.log("%cDeviceMotion is not supported.", 'color: red');
+			console.log("Did you run Motion.init function?");
+		}
+	};
+
+	/**
+	 * Function removes DeviceMotionEvent listener - only if it is supported and added before.
+	 * @public
+	 */
+	Motion.finish = function() {
+		if (isSupported && isRunning) {
+			isRunning = false;
+			window.removeEventListener("devicemotion", deviceMotionHandler, false);
+		}
+	};
+
+	/**
+	 * Handler for DeviceMotionEvent.
+	 * Reads values from the event and sends to the server.
+	 * @private
+	 * @param  {DeviceMotionEvent} e
+	 */
+	function deviceMotionHandler(e) {
+		// when phone is in typical position for this app - face to the user, top of the phone to the left, left side is going down
+		// x - up, down
+		// y - left, right
+		// z - forward, backward
+
+		let divisor = 2;
+
+		if (sequence++ % divisor === 0) {
+			tmpX = e.acceleration.x;
+			tmpY = e.acceleration.y;
+			tmpZ = e.acceleration.z;
+			checkError(e);
+		} else {
+			let xToSend = (tmpX + e.acceleration.x) / divisor;
+			let yToSend = (tmpY + e.acceleration.y) / divisor;
+			let zToSend = (tmpZ + e.acceleration.z) / divisor;
+
+			let request = new XMLHttpRequest();
+			request.open('POST', address);
+			request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			let obj = {
+				type: "motion",
+				time: new Date().getTime(),
+				sequence: sequence / divisor,
+				x: xToSend,
+				y: yToSend,
+				z: zToSend
+			};
+			request.send(JSON.stringify(obj));
+		}
+	}
+
+	/**
+	 * Checks if values of DeviceMotionEvent are "normal".
+	 * @private
+	 * @param  {DeviceMotionEvent} e
+	 */
+	function checkError(e) {
+		if ((e.acceleration.x === null || e.acceleration.y === null || e.acceleration.z === null) && !errorShown) {
+			let msg = "This device doesn't have all necessary sensors to determine its motion.";
+			console.log("%c" + msg, 'color: red');
+			window.alert(msg);
+			window.removeEventListener("devicemotion", deviceMotionHandler, false);
+			errorShown = true;
+			isSupported = false;
+			isRunning = false;
+		}
+	}
+
+	// export Motion object
+	return Motion;
+})();
--- server.js
+++ server.js
@@ -19,12 +19,14 @@ const serveStatic = require('serve-static');
 
 // setup server with all important resources and listeners for ajax calls
 const server = express()
+	.use(serveStatic(__dirname, {'alphabet_receiver': false}))
+	.use(serveStatic(__dirname, {'camera': false}))
+	.use(serveStatic(__dirname, {'controller': false}))
 	.use(serveStatic(__dirname, {'receiver': false}))
 	.use(serveStatic(__dirname, {'rotation': false}))
-	.use(serveStatic(__dirname, {'camera': false}))
-	.use(serveStatic(__dirname, {'detection': false}))
 	.use(bodyParser.json({limit: '5mb'}))
 	.post('/ajax/rotation', receiveData)
+	.post('/ajax/motion', receiveData)
 	.post('/ajax/marker', receiveData)
 	.post('/ajax/touch', receiveData)
 	.post('/ajax/picture', receivePicture)
